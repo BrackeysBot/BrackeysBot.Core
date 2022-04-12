d2f05d971390ee2e25fe089e0fd4e374cb7d0a89
Oliver Booth <me@olivr.me>
2022-04-12 12:02:27 +0100

    Add SlashRequirePermissionLevelAttribute
    Bumps BrackeysBot.API to 4.0.0-nightly.17

---
+++ BrackeysBot.Core.API/Attributes/SlashRequirePermissionLevelAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.SlashCommands;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BrackeysBot.Core.API.Attributes;
+
+/// <summary>
+///     Defines that usage of this command must require a specified minimum <see cref="PermissionLevel" />.
+/// </summary>
+public sealed class SlashRequirePermissionLevelAttribute : SlashCheckBaseAttribute
+{
+    private readonly PermissionLevel _permissionLevel;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="RequirePermissionLevelAttribute" /> class.
+    /// </summary>
+    /// <param name="permissionLevel">The minimum permission level.</param>
+    public SlashRequirePermissionLevelAttribute(PermissionLevel permissionLevel)
+    {
+        _permissionLevel = permissionLevel;
+    }
+
+    /// <inheritdoc />
+    public override Task<bool> ExecuteChecksAsync(InteractionContext ctx)
+    {
+        if (ctx.Guild is null) return Task.FromResult(false);
+
+        var corePlugin = ctx.Services.GetRequiredService<ICorePlugin>();
+        return Task.FromResult(corePlugin.GetPermissionLevel(ctx.User, ctx.Guild) >= _permissionLevel);
+    }
+}
